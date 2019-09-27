e068e1912c55932bd21d19ee338c01a0cf9d7625
ttgc <ttgc@users.noreply.github.com>
2019-09-27 13:40:48 -0400

    Added common library files

---
+++ main/common/include/network.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef _WIN32
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+#else
+
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <errno.h>
+
+using SOCKET = int;
+#define SD_BOTH SHUT_RDWR
+#define closesocket(s) close(s)
+#endif
