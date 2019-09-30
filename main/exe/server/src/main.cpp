48d1c129481b3811708c4ab0c5f0200abf06cadd
ttgc <ttgc@users.noreply.github.com>
2019-09-30 15:02:54 -0400

    Created basic run structure in server exe

--- main/exe/server/src/main.cpp
+++ main/exe/server/src/main.cpp
@@ -1,8 +1,33 @@
 #include "thunderchatserver.hpp"
 #include <iostream>
+#include <chrono>
+
+#ifdef _WIN32
+#include "winnetworkconfig.hpp"
+#endif // WIN32
 
 int main(void)
 {
-    std::cout << "Hello world" << std::endl;
-    std::cin.ignore();
+#ifdef _WIN32
+	network::WinNetworkConfig api;
+#endif // WIN32
+
+    std::cout << "Hello world!" << std::endl;
+
+	server::ThunderChatServer server("127.0.0.1", 8888);
+	if (server.isRunning())
+	{
+		using namespace std::chrono_literals;
+		std::cout << "Server is running on : " << server.getIP() << ":" << server.getPort() << "\n";
+		server.onConnect([](const std::string& client) { std::cout << client << " is now online\n"; });
+		server.onDisconnect([](const std::string& client) { std::cout << client << " is now offline\n"; });
+		std::this_thread::sleep_for(10s);
+	}
+	else
+	{
+		std::cout << "FATAL ERROR : " << server.getErrorCode() << "\n";
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
