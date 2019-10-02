17f7777598cffb8aa13d4311045bee6064515819
Cygha <tristan.bechu@gmail.com>
2019-10-02 15:12:24 -0400

    Add DNS main.cpp

--- main/myexe/src/main.cpp
+++ main/myexe/src/main.cpp
@@ -1,4 +1,5 @@
 #include "ThunderChatClient.hpp"
+#include "dnsresolver.hpp"
 #include <iostream>
 
 #ifdef _WIN32
@@ -14,34 +15,37 @@ int main(void)
 
     std::string ip;
     std::string username;
-    std::string teamChose;
+    std::string teamChoice;
     network::message::Team teamUser;
 
     std::cout << "Bonjour et bienvenue dans le chat de Thunder Siege 6 ! " << std::endl;
     std::cout << "Veuillez rentrer dans l'ordre :" << std::endl
-              << "1.L'adresse ip sur laquelle vous connecter." << std::endl;
+              << "1.L'adresse du serveur sur laquelle vous connecter." << std::endl;
     std::cin >> ip;
     std::cout << "2. Le pseudonyme que vous souhaitez utiliser." << std::endl;
     std::cin >> username;
-    std::cout << "3. l'équipe que vous souhaitez rejoindre." << std::endl;
+    std::cout << "3. l'Equipe que vous souhaitez rejoindre." << std::endl;
     std::cout << "A : Equipe A" << std::endl;
     std::cout << "B : Equipe B" << std::endl;
-    std::cin >> teamChose;
+    std::cin >> teamChoice;
 
-	if (teamChose == "A") 
+    network::DNSresolver::DNSresolver(ip);
+    std::vector<sockaddr_in> vect;
+    vect = network::DNSresolver::all();
+    if(vect != nullptr) ip = vect.end();
+
+	if (teamChoice == "A") 
 	{ 
 		teamUser = network::message::TEAM_A; }
-    else if (teamChose == "B")
+    else if (teamChoice == "B")
     {
         teamUser = network::message::TEAM_B;
     }
-    
 
     client::ThunderChatClient client(ip, username, teamUser);
 
     if (client.Connect())
     {
-
         std::cin.ignore();
         client.SendToParty("Test send to party");
     }
