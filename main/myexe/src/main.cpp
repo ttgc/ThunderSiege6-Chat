ff7bcd1ce282283a5ed9c91c5ccff053cb00025f
Taigo <TaigoFR92@gmail.com>
2019-10-02 13:51:36 -0400

    Début du main
    Demande de l'IP, username, et Team, puis connection avec les variable

--- main/myexe/src/main.cpp
+++ main/myexe/src/main.cpp
@@ -1,24 +1,48 @@
-#include <iostream>
 #include "ThunderChatClient.hpp"
+#include <iostream>
 
 #ifdef _WIN32
 #include "winnetworkconfig.hpp"
 #endif // WIN32
 
-
 int main(void)
 {
-    
+
 #ifdef _WIN32
-		network::WinNetworkConfig api;
+    network::WinNetworkConfig api;
 #endif // WIN32
 
+    std::string ip;
+    std::string username;
+    std::string teamChose;
+    network::message::Team teamUser;
 
-		client::ThunderChatClient client("192.168.0.133", "JeanMi", network::message::TEAM_A);
+    std::cout << "Bonjour et bienvenue dans le chat de Thunder Siege 6 ! " << std::endl;
+    std::cout << "Veuillez rentrer dans l'ordre :" << std::endl
+              << "1.L'adresse ip sur laquelle vous connecter." << std::endl;
+    std::cin >> ip;
+    std::cout << "2. Le pseudonyme que vous souhaitez utiliser." << std::endl;
+    std::cin >> username;
+    std::cout << "3. l'équipe que vous souhaitez rejoindre." << std::endl;
+    std::cout << "A : Equipe A" << std::endl;
+    std::cout << "B : Equipe B" << std::endl;
+    std::cin >> teamChose;
 
-		if (client.Connect()) { 
+	if (teamChose == "A") 
+	{ 
+		teamUser = network::message::TEAM_A; }
+    else if (teamChose == "B")
+    {
+        teamUser = network::message::TEAM_B;
+    }
+    
 
-			std::cin.ignore();
-			client.SendToParty("Test send to party");
-		}
+    client::ThunderChatClient client(ip, username, teamUser);
+
+    if (client.Connect())
+    {
+
+        std::cin.ignore();
+        client.SendToParty("Test send to party");
+    }
 }
